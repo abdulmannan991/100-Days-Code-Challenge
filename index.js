29eab5c44350b3c2b30a10d804756642ec3340e6
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-01 17:09:23 +0500

    1 day of code

---
+++ index.js
@@ -0,0 +1,8 @@
+// Question # 01
+// let person_name = "Abdul Mannan would you like to learn some TypeScript today?";
+// console.log(person_name);
+// Question # 02
+var new_person = "Abdul Mannan";
+console.log(new_person.toLowerCase());
+console.log(new_person.toUpperCase());
+console.log(new_person.charAt(0).toUpperCase() + new_person.slice(1).toLowerCase());
