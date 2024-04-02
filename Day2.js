9bc86d3f1290202f8f047855b115d2eb6d062844
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-03 00:36:46 +0500

    day 2

---
+++ Day2.js
@@ -0,0 +1,16 @@
+// Question 1 
+console.log("Quaid - e - Azam Muhammad Ali Jinnah once said:-\"Work work and work and we are bound to success\"");
+// Question 2
+var famous_person = "Quaid - e - Azam Muhammad Ali Jinnah once said:-";
+var famouos_Quote = "\"Work work and work and we are bound to success\"";
+// Method 1
+var message = famous_person + famouos_Quote;
+// Method 2
+console.log(famous_person + "" + famouos_Quote);
+console.log(message);
+// Question 4
+var my_name = "\t\n Abdul Mannan \t\n";
+// This show the name with whitespace
+console.log(my_name);
+// This shows the name without whitespcae
+console.log(my_name.trim());
---
+++ Day2.ts
@@ -0,0 +1,36 @@
+
+
+
+// Question 1 
+
+console.log("Quaid - e - Azam Muhammad Ali Jinnah once said:-\"Work work and work and we are bound to success\"")
+
+
+
+// Question 2
+let famous_person = "Quaid - e - Azam Muhammad Ali Jinnah once said:-";
+
+let famouos_Quote = "\"Work work and work and we are bound to success\"";
+
+// Method 1
+const message = famous_person + famouos_Quote;
+
+// Method 2
+
+console.log(famous_person + "" + famouos_Quote);
+
+console.log(message);
+
+// Question 4
+
+let my_name = "\t\n Abdul Mannan \t\n";
+
+
+// This show the name with whitespace
+console.log(my_name);
+
+// This shows the name without whitespcae
+console.log(my_name.trim());
+
+
+
