d1e59730511b21c76cd614c44b645824f7eef1b0
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-03 23:43:24 +0500

    day 3

---
+++ 3rd-day.ts
@@ -0,0 +1,23 @@
+
+// Question 7
+
+// Addition
+console.log(2+6);
+
+// Subtraction
+console.log(10-2);
+
+// Multiplication
+console.log(2*4);
+
+// Division
+console.log(16/2);
+
+// Question 8 
+
+// Same as Question 7
+
+// Question 9
+
+const Favorite_number = 22;
+console.log('My Favorite Number is :-'+ Favorite_number);
