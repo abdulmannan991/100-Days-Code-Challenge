79ba4336f6d416bf6bfa108b1c3e0d8679946ff8
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-04 15:27:55 +0500

    Day 4 done!

---
+++ day4.js
@@ -0,0 +1,22 @@
+// Question 10
+// First, I declare two variables.
+var num1 = 5;
+var num2 = 15;
+console.log("Add Two numbers :- ", num1 + num2);
+console.log("______________________________________");
+// // Then, I initialize each variable with a value.
+// After that, I perform a simple addition operation,
+//  This operation add both the numbers and prform a simple task.
+// Question 11 
+var my_array = ["Haris", "Hammad", "Hamza", "Hamid"];
+for (var i = 0; i < my_array.length; i++) {
+    var element = my_array[i];
+    console.log(my_array[i]);
+}
+console.log("______________________________________");
+// Question 12
+var my_array1 = ["Haris", "Hammad", "Hamza", "Hamid"];
+for (var i = 0; i < my_array.length; i++) {
+    var element = my_array[i];
+    console.log("Welcome :- " + my_array1[i]);
+}
---
+++ day4.ts
@@ -0,0 +1,34 @@
+
+// Question 10
+
+// First, I declare two variables.
+let num1 = 5;
+let num2 = 15 ;
+
+console.log("Add Two numbers :- ", num1+num2);
+console.log("______________________________________");
+
+// // Then, I initialize each variable with a value.
+// After that, I perform a simple addition operation,
+//  This operation add both the numbers and prform a simple task.
+
+// Question 11 
+
+let my_array = ["Haris" , "Hammad", "Hamza" , "Hamid" ];
+for (let i = 0; i < my_array.length; i++) {
+    const element = my_array[i];
+    console.log(my_array[i]);
+}
+console.log("______________________________________");
+
+// Question 12
+
+let my_array1 = ["Haris" , "Hammad", "Hamza" , "Hamid" ];
+for (let i = 0; i < my_array.length; i++) {
+    const element = my_array[i];
+    console.log("Welcome :- " + my_array1[i]);
+
+}
+
+
+
