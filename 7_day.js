f56356e4ec084584d8382fe614b64ca6665e2bed
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-08 15:10:49 +0500

    7 day

---
+++ 7_day.js
@@ -0,0 +1,13 @@
+// Question 19
+var guests = ["Sarim", "Asim", "Razzaq", "Hammad", "Fakhir", "Faheem"];
+console.log(guests.length);
+//  Question 20
+var cities = ["Hunza", "Kashmir", "Sawat", "Skardu"];
+console.log("I would like to visit this places", cities);
+// Question 21 
+var book = {
+    title: "Peace of river",
+    author: "Williams Words",
+    Publishedyear: 1965,
+};
+console.log("Book info :", "Book title :", book.title, "By", book.author, "Published in", book.Publishedyear);
---
+++ 7_day.ts
@@ -0,0 +1,22 @@
+
+// Question 19
+
+ let guests  = ["Sarim","Asim","Razzaq","Hammad","Fakhir","Faheem"];
+ console.log(guests.length);
+
+
+//  Question 20
+
+let cities = ["Hunza","Kashmir","Sawat","Skardu"];
+console.log("I would like to visit this places",cities);
+
+// Question 21 
+
+let book : {title : String ; author: String; Publishedyear : number } = {
+
+    title : "Peace of river",
+    author : "Williams Words",
+    Publishedyear : 1965,
+}
+
+console.log ("Book info :", "Book title :",book.title,"By" ,book.author,"Published in", book.Publishedyear)
