a5ffe245b28e3717fba38882864f7d05b679d42c
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-08 16:41:32 +0500

    day 9

---
+++ 9_day.js
@@ -0,0 +1,28 @@
+// Question 25
+var alien_color1 = "Green";
+var alien_color2 = "Yellow";
+var alien_color3 = "Red";
+if (alien_color1 == "Green") {
+    console.log("Congratulations! You earned 5 points. ");
+}
+if (alien_color3 == "green") {
+}
+console.log("_____________________________________");
+// Question 26
+if (alien_color2 == "Yellow") {
+    console.log("Congratulations! You earned 15 points ");
+}
+else {
+    console.log("You earned 0 points :(");
+}
+console.log("_____________________________________");
+// Question 27 
+if (alien_color1 == "Yellow") {
+    console.log("You earend 15 points!");
+}
+else if (alien_color3 == "Red") {
+    console.log("You earend 10 points!");
+}
+else {
+    console.log("Yor earned 5 points!");
+}
---
+++ 9_day.ts
@@ -0,0 +1,34 @@
+
+// Question 25
+
+let alien_color1 = "Green";
+let alien_color2 = "Yellow";
+let alien_color3 = "Red";
+
+if (alien_color1 == "Green") {
+    console.log("Congratulations! You earned 5 points. ")
+}  if(alien_color3 == "green") {
+  
+}
+console.log("_____________________________________");
+
+// Question 26
+
+if (alien_color2 == "Yellow") {
+    console.log("Congratulations! You earned 15 points ");
+} else {
+    console.log("You earned 0 points :(");
+}
+
+console.log("_____________________________________");
+// Question 27 
+
+if (alien_color1 == "Yellow") {
+    console.log("You earend 15 points!");
+} 
+else if(alien_color3 == "Red"){
+console.log("You earend 10 points!")
+}
+else {
+    console.log("Yor earned 5 points!")
+}
