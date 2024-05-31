d122befc40a25a66aae4e4482e404c87bff84d8e
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-05-31 21:07:35 +0500

    Add files via upload
    17 day done

---
+++ 17.js
@@ -0,0 +1,21 @@
+// Question 49
+function Hobbies(Reading, Playing, Jogging) {
+    console.log("Is your hobby  ".concat(Reading, "?"));
+    console.log("Is your hobby  ".concat(Playing, "?"));
+    console.log("Is your hobby  ".concat(Jogging, "?"));
+}
+;
+Hobbies("Reading", "Playing", "Joging");
+// Question 50 
+console.log("_____________________________________");
+var myIdealDay = "My ideal day would involve:\n1. Waking up early and going for a University.\n2. Spending a few hours coding on a personal project.\n3. Ending the day by reading a good book.";
+console.log(myIdealDay);
+// Question 51
+console.log("_____________________________________");
+var RectangularArea = function () {
+    var width = 5;
+    var height = 10;
+    var result = width * height;
+    console.log(result);
+};
+RectangularArea();
---
+++ 17.ts
@@ -0,0 +1,34 @@
+// Question 49
+
+function Hobbies (Reading:String, Playing :String , Jogging : String) {
+    console.log(`Is your hobby  ${Reading}?`);
+    console.log(`Is your hobby  ${Playing}?`);
+    console.log(`Is your hobby  ${Jogging}?`);
+};
+
+
+Hobbies("Reading","Playing","Joging");
+
+// Question 50 
+console.log("_____________________________________");
+
+
+let myIdealDay = `My ideal day would involve:
+1. Waking up early and going for a University.
+2. Spending a few hours coding on a personal project.
+3. Ending the day by reading a good book.`;
+
+console.log(myIdealDay);
+
+
+// Question 51
+console.log("_____________________________________");
+
+let RectangularArea = () => {
+    let width = 5;
+    let height = 10;
+    let result = width * height;
+    console.log(result);
+}
+
+RectangularArea()
