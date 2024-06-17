afd534fa921d914e068428b3e003800bd41becfb
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-06-17 11:28:10 +0500

    day 28

---
+++ 28.js
@@ -0,0 +1,18 @@
+// Question 82
+function StringFunction(value) {
+    return value;
+}
+console.log(StringFunction("Myname").length);
+// Question 83
+function LoweToUpperCase() {
+    var value = "heLLo WoRld";
+    return value;
+}
+console.log(LoweToUpperCase().toUpperCase());
+console.log(LoweToUpperCase().toLowerCase());
+// Question 84
+function ReplaceWord() {
+    var value = "Hello World!";
+    return value.replace("Hello World!", "Bye World!");
+}
+console.log(ReplaceWord());
---
+++ 28.ts
@@ -0,0 +1,29 @@
+// Question 82
+
+function StringFunction(value : string){
+     return value;
+}
+
+console.log(StringFunction("Myname").length);
+
+// Question 83
+
+function LoweToUpperCase(){
+    let value = "heLLo WoRld";
+return value;
+}
+
+console.log(LoweToUpperCase().toUpperCase());
+console.log(LoweToUpperCase().toLowerCase());
+
+// Question 84
+
+function ReplaceWord(){
+
+    let value = "Hello World!";
+   return  value.replace("Hello World!","Bye World!");
+}
+
+console.log(ReplaceWord());
+
+
