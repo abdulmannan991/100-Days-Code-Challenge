35f2099100aede5ab5cad5dafe5b5a8894e710b3
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-08-11 16:52:22 +0500

    day 34

---
+++ 34.js
@@ -0,0 +1,13 @@
+// Question 100
+function SquareRoot(number) {
+    console.log(Math.sqrt(number));
+}
+SquareRoot(4);
+// Question 101
+function RandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(RandomNumber());
+// Question 102
+var numbers = Math.abs(-6 + 6);
+console.log(numbers);
---
+++ 34.ts
@@ -0,0 +1,23 @@
+// Question 100
+
+function SquareRoot(number : number){
+
+    
+    console.log(Math.sqrt(number));
+}
+
+SquareRoot(4);
+
+// Question 101
+
+function RandomNumber(): number {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(RandomNumber()); 
+
+
+// Question 102
+
+const numbers = Math.abs(-6 +6)
+console.log(numbers);
