4ced452d3953534ad9147e0e4ebef17eb26ac81b
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-08-24 13:44:35 +0500

    day 40

---
+++ 40.js
@@ -0,0 +1,17 @@
+// Question 118
+for (var i_1 = 1; i_1 <= 10; i_1++) {
+    console.log(i_1);
+}
+// Question 119
+console.log("____________________________");
+var i = 1;
+while (i <= 5) {
+    i++;
+    console.log("Hello World!");
+}
+// Question 120
+console.log("____________________________");
+var NoMovies = ["Movie1", "Movie2", "Movie3", "Movie4"];
+for (var i_2 = 0; i_2 < NoMovies.length; i_2++) {
+    console.log(NoMovies[i_2]);
+}
---
+++ 40.ts
@@ -0,0 +1,28 @@
+// Question 118
+
+for(let i = 1 ; i <=10; i++){
+    console.log(i);
+}
+
+// Question 119
+console.log("____________________________");
+
+let i = 1;
+while(i<=5){
+ i++;
+ console.log("Hello World!");
+
+}
+
+// Question 120
+
+console.log("____________________________");
+
+let NoMovies = ["Movie1","Movie2","Movie3","Movie4"]
+for(let i = 0; i < NoMovies.length; i++){
+      console.log(NoMovies[i])
+}
+
+
+
+
